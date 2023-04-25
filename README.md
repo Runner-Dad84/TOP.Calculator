3f468164c09ed46fbb1e038054a431e150763182
Runner-Dad84 <vanhoogie@gmail.com>
2023-04-24 22:04:07 -0400

    Create html, js and style sheets. Create basic functions, add, sub, mult, div.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # TOP.Calculator
 The Odin Project Calculator
+Final project of the introduction lessions. 
+
+Part 1:
+Basic calculator function. 
+Create the following functions in js:
+    ADD
+    SUBSTRACT
+    MULTIPLY
+    DIVIDE
---
+++ script.js
@@ -0,0 +1,17 @@
+//Functions
+
+function addNum (a, b) {
+    return a + b;
+}
+
+function subtractNum (a, b) {
+    return a - b;
+}
+
+function multiplyNum (a, b) {
+    return a * b;
+}
+
+function divideNum (a, b) {
+    return a / b;
+}
