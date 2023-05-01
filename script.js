33a6983b6560c5aade4a406805d075ba08819dac
Runner-Dad84 <vanhoogie@gmail.com>
2023-04-30 21:50:50 -0400

    Add operator function.

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ function addNum (a, b) {
     return a + b;
 }
 
-function subtractNum (a, b) {
+function subtrNum (a, b) {
     return a - b;
 }
 
-function multiplyNum (a, b) {
+function multNum (a, b) {
     return a * b;
 }
 
-function divideNum (a, b) {
+function divNum (a, b) {
     return a / b;
 }
 
@@ -30,11 +30,34 @@ let displayValue = "";
 const numbers = document.querySelectorAll(".number");
 const display = document.getElementById("display");
 const clear = document.getElementById("clear");
+const operators = document.querySelectorAll(".operator");
 
 numbers.forEach(number => {
     number.addEventListener("click", () => {
         display.textContent += number.textContent;
         displayValue += number.textContent;
+    });
+});
+
+operators.forEach(oper => {
+    oper.addEventListener("click", () => {
+        display.textContent += oper.textContent;
+        let tempOper = oper.textContent;
+        switch (true) {
+            case tempOper === "+":
+                operator = addNum;
+                break;
+            case tempOper === "-":
+                operator = subtrNum;
+                break;
+            case tempOper === "*":
+                operator = multNum;
+                break;
+            case tempOper === "/":
+                operator = divNum;
+                break;
+        };
+        return operator;
     });
 });
 
