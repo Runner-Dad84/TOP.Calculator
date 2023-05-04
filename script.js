8547f56f2eb172d4b90d895669cb48886be0fd20
Runner-Dad84 <vanhoogie@gmail.com>
2023-05-03 22:10:33 -0400

    Values stored as numbers for first and second mumber inout.

--- script.js
+++ script.js
@@ -1,67 +1,101 @@
-//Functions
-
-function addNum (a, b) {
-    return a + b;
-}
-
-function subtrNum (a, b) {
-    return a - b;
-}
-
-function multNum (a, b) {
-    return a * b;
-}
-
-function divNum (a, b) {
-    return a / b;
-}
-
-//input values
+//global variables
 let operator;
-let numOne;
-let numTwo;
+let numOne = "";
+let numTwo = "";
+let total;
+let operatorValue;
+let displayValue = "";
 
-function operate(displayValue, numTwo, operator) {
-    return operator (displayValue, numTwo);
+
+function addNum (numOne, numTwo){
+    total = numOne + numTwo;
+};
+
+
+/*
+function operate(numTwo, numTwo, operator) {
+    switch (operator) {
+        case operator === addNum():
+            addNum();
+            
+        break;
+        case operator === subtrNum:
+            function subtrNum (numOne, numTwo){
+            total = numOne - numTwo;
+            return total;
+            };
+        break;
+        case operator === multNum:
+            function multNum (numOne, numTwo){
+            total = numOne * numTwo;
+            return total;
+            };
+        case operator === divNum:
+            function divNum (numOne, numTwo) {
+            total = numOne / numTwo;
+            return total;
+            }
+        break;
+    };
+    
 }
+*/
 
 // input numbers to display
-let displayValue = "";
 const numbers = document.querySelectorAll(".number");
 const display = document.getElementById("display");
 const clear = document.getElementById("clear");
 const operators = document.querySelectorAll(".operator");
+const equals = document.getElementById("equals");
 
+//numbers 1 and 2//
 numbers.forEach(number => {
     number.addEventListener("click", () => {
+        if(operatorValue === "+"){
+            display.textContent += number.textContent;
+            displayValue += number.textContent;
+            numTwo = Number(displayValue);
+            return(numTwo);
+    } else {
         display.textContent += number.textContent;
-        displayValue += number.textContent;
-        numOne = parseInt(displayValue);
-    });
-});
-
+            displayValue += number.textContent;
+            numOne = Number(displayValue);
+            return(numOne);
+    }
+})});
+//Operator click//
 operators.forEach(oper => {
     oper.addEventListener("click", () => {
         display.textContent += oper.textContent;
         let tempOper = oper.textContent;
+        display.textContent = "";
+        displayValue = "";
         switch (true) {
             case tempOper === "+":
-                operator = addNum;
+                operator = addNum();
+                operatorValue = "+"
                 break;
             case tempOper === "-":
-                operator = subtrNum;
+                operator = subtrNum();
+                operatorValue = "-"
                 break;
             case tempOper === "*":
-                operator = multNum;
+                operator = multNum();
+                operatorValue = "*"
                 break;
             case tempOper === "/":
-                operator = divNum;
+                operator = divNum();
+                operatorValue = "/"
                 break;
         };
-        return operator;
     });
 });
-
+//Equals//
+equals.addEventListener("click", () => {
+       display.textContent += equals.textContent; 
+        console.log(total);
+        
+});
 
 //Clear All
 clear.addEventListener("click", () => {
